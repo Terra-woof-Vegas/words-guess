44041897bdde4b0f83cf36d1b7b70697fdf963b0
9lanx1N <fakesimchain@gmail.com>
2022-08-06 16:46:14 +0800

    使Random_words列对齐

--- Random_words.py
+++ Random_words.py
@@ -1,17 +1,44 @@
 import random
 import sys
 
-filename = 'arknights.txt'
-wordlist = []
-f = open(filename, 'r', encoding='utf-8')
-for line in f:
-    if line[0] != '#':
-        wordlist.append(line.strip('\n'))
-result = random.sample(wordlist, 25)
 
-cnt = 0
-for i in range(25):
-    if i % 5 == 4:
-        print(result[i])
-    else:
-        print(result[i], end=" ")
+def random_words(filename):
+    """
+    读取词库文件，并选出25个词
+    :param filename: 词库文件名称
+    :return: 不重复的25词
+    """
+    wordlist = []
+    f = open(filename, 'r', encoding='utf-8')
+    for line in f:
+        if line[0] != '#':
+            wordlist.append(line.strip('\n'))
+    result = random.sample(wordlist, 25)
+    return result
+
+
+def print_words(words):
+    """
+    打印词表
+    :param words: 25个词
+    :return: 5*5格式的词表
+    """
+    longest = 0
+    for w in words:
+        longest = max(longest, len(w))
+
+    for i in range(25):
+        if i % 5 == 4:
+            print(words[i])
+        else:
+            cnt = 0
+            for ch in words[i]:
+                if not '\u4e00'<= ch <= '\u9fff':
+                    cnt = cnt + 1
+            if cnt == 0:
+                print('{0:{1}<{2}}'.format(words[i],chr(12288),longest+1), end="")
+            else:
+                words[i] = words[i].ljust(len(words[i])+cnt, " ")
+                print('{0:{1}<{2}}'.format(words[i],chr(12288),longest+cnt+1), end="")
+
+
