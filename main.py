64e17d93cf4b1ada6f96f785e01635b60154d8f1
Theraphia Li <54740400+Theraphia@users.noreply.github.com>
2022-08-06 15:11:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import Random_color
 
 # filename = input("请选择词库:")
 print("-----------------------------")
-w = Random_words.random_words("wordrepo/arknights.txt")
+w = Random_words.random_words("arknights.txt")
 Random_words.print_words(w)
 print("-----------------------------")
 Random_color.random_color()
