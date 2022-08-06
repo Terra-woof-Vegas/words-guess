d9a4ab4753b4d58cee422f6cacc00381d626fd16
Theraphia Li <54740400+Theraphia@users.noreply.github.com>
2022-08-06 15:09:31 +0800

    主函数，可以直接使用

---
+++ main.py
@@ -0,0 +1,10 @@
+import Random_words
+import Random_color
+
+# filename = input("请选择词库:")
+print("-----------------------------")
+w = Random_words.random_words("wordrepo/arknights.txt")
+Random_words.print_words(w)
+print("-----------------------------")
+Random_color.random_color()
+print("-----------------------------")
