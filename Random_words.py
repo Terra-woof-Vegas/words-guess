17f84de57598beff4a49a807a61c586358418147
Theraphia Li <54740400+Theraphia@users.noreply.github.com>
2022-08-06 14:42:14 +0800

    改为函数

--- Random_words.py
+++ Random_words.py
@@ -1,17 +1,31 @@
 import random
 import sys
 
-filename = 'arknights.txt'
-wordlist = []
-f = open(filename, 'r', encoding='utf-8')
-for line in f:
-    if line[0] != '#':
-        wordlist.append(line.strip('\n'))
-result = random.sample(wordlist, 25)
 
-cnt = 0
-for i in range(25):
-    if i % 5 == 4:
-        print(result[i])
-    else:
-        print(result[i], end=" ")
+def random_words(filename):
+    """
+    读取词库文件，并选出25个词
+    :param filename: 词库文件名称
+    :return: 不重复的25词
+    """
+    wordlist = []
+    f = open(filename, 'r', encoding='utf-8')
+    for line in f:
+        if line[0] != '#':
+            wordlist.append(line.strip('\n'))
+    result = random.sample(wordlist, 25)
+    return result
+
+
+def print_words(words):
+    """
+    打印词表
+    :param words: 25个词
+    :return: 5*5格式的词表
+    """
+    cnt = 0
+    for i in range(25):
+        if i % 5 == 4:
+            print(words[i])
+        else:
+            print(words[i], end=" ")
